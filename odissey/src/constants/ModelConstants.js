fc04180ab6c506e15304ffc6df76e93359f28282
Andrea Ritossa <andre.ritossa@gmail.com>
2025-02-06 18:21:17 +0100

    llm constant fix for hf inferece

--- odissey/src/constants/ModelConstants.js
+++ odissey/src/constants/ModelConstants.js
@@ -1,7 +1,7 @@
 import { HF_API_TOKEN_READ } from '@env';
 
 const HF_API_TOKEN = HF_API_TOKEN_READ;
-const NARRATOR_MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"; // "google/gemma-2-2b-it";//"HuggingFaceTB/SmolLM2-1.7B-Instruct";
+const NARRATOR_MODEL_NAME = "meta-llama/Llama-3.2-3B-Instruct"; // "google/gemma-2-2b-it";//"HuggingFaceTB/SmolLM2-1.7B-Instruct";
 const SPEECH_RECOGNITION_MODEL_NAME = "openai/whisper-large-v3-turbo"; //"openai/whisper-large-v3-turbo";
 const TEXT_TO_AUDIO_MODEL_NAME = "facebook/fastspeech2-en-ljspeech"; //"suno/bark";
 
